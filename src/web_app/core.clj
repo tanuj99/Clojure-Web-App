6a1f1f5102592f55998fbbcf41708c1a82f1faaa
tanuj99 <tanujsoni08@gmail.com>
2024-02-19 19:37:28 +0530

    Add middlewares and routes to the web app

--- src/web_app/core.clj
+++ src/web_app/core.clj
@@ -2,7 +2,9 @@
   (:require [ring.adapter.jetty :as jetty]
             [clojure.pprint :as pprint]
             [compojure.core :as comp]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]))
 
 ;; (defn foo
 ;;   "I don't do a whole lot."
@@ -11,26 +13,32 @@
 
 (defonce server (atom nil)) ;; defonce instead of def, when we reload the file, it won't overwrite the variable
 
-(defn app [req]
-  (case (:uri req)
-    "/" {:status 200
-         :body "<h1>Homepage</h1>
-                <ul>
-                    <li><a href=\"/echo\">Echo request</a></li>
-                    <li><a href=\"/greeting\">Greeting</a></li>
-                </ul>"
-         :headers {"Content-Type" "text/html; charset=UTF-8"}}
-    "/echo" {:status 200
-             :body (with-out-str (pprint/pprint req)) ;; pretty print the ring request 
-             :headers {"Content-Type" "text/plain"}}
-    "/greeting" {:status 200
-                 :body "Hello, World!"
-                 :headers {"Content-Type" "text/plain"}}
-    {:status 404
-     :body "404 Not Found! Error!!!!" ;; pretty print the ring request 
-     :headers {"Content-Type" "text/plain"}}))
+;; (defn app [req])
 
-(comp/defroutes app)
+(comp/defroutes routes
+  (comp/GET "/" [] {:status 200
+                    :body "<h1>Homepage</h1>
+                             <ul>
+                                 <li><a href=\"/echo\">Echo request</a></li>
+                                 <li><a href=\"/greeting\">Greeting</a></li>
+                             </ul>"
+                    :headers {"Content-Type" "text/html; charset=UTF-8"}})
+  (comp/ANY "/echo" req {:status 200
+                         :body (with-out-str (pprint/pprint req)) ;; pretty print the ring request 
+                         :headers {"Content-Type" "text/plain"}})
+  (comp/GET "/greeting" [] {:status 200
+                            :body "Hello, World!"
+                            :headers {"Content-Type" "text/plain"}})
+  (route/not-found {:status 404
+                    :body "404 Not Found! Error!!!!" ;; pretty print the ring request 
+                    :headers {"Content-Type" "text/plain"}}))
+
+
+#_(def app (wrap-params (wrap-keyword-params routes)))
+(def app
+  (-> routes
+      wrap-keyword-params
+      wrap-params))
 
 
 
